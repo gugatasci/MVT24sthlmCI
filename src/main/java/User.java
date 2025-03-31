78c0cf3c7290147ec75085a2c2fef0da5f84ca2f
gugatasci <gugatasci@hotmail.com>
2025-03-31 11:52:06 +0200

    first commit

---
+++ src/main/java/User.java
@@ -0,0 +1,7 @@
+public class User {
+    private String user,password;
+
+    User(String user,String pwd){
+
+    }
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+//V10 uppgift CI
+
+public class UserTest {
+
+    @Test
+    public void addUser(){
+
+        User user = new User ("Kalle","abc123");
+
+    }
+}
