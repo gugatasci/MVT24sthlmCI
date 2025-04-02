5ddf2d05751800745d3cb63fa1dc1f24e96a42b3
gugatasci <gugatasci@hotmail.com>
2025-04-02 09:05:12 +0200

    3 commit

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,14 @@
 public class User {
     private String user,password;
 
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     User(String user, String pwd){
         this.user = user;
         this.password = pwd;
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
+    private User user;
+
     @Test
     public void addUserTest(){
         User user = new User("Kalle","abc123");
@@ -19,5 +21,13 @@ public class UserTest {
     public void getPasswordTest(){
         User user = new User("Kalle","abc123");
         assertEquals("abc123",user.getPassword());
+    }
+    @Test
+    public void changeUserTest(){
+
+        User user = new User("Mehmet","abc123");
+        this.user = user;
+        assertEquals("Mehmet",user.getUser());
+
     }
 }
